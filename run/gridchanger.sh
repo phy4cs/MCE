e5b51b2b56895a098e601424dbf4844d3317a04c
Chris on CHMLIN40 <phy4cs@leeds.ac.uk>
2015-03-03 19:28:12 +0000

    Altered the MainMCE file to allow the simultaneous use of OpenMP and adaptive stepsizes (previously the reduction variables were un-allocated which was causing problems). Also changed the run.sh file to prevent overwriting of the input.dat file, meaning that a new commit is not needed when the program is run. Included code in the run.sh file to allow the grid changer to be used on chmlin45 ONLY (this may need to be removed at a later date and so have been marked by comments). The timestrt variable has had a local version included in the MainMCE file to prevent any conflicts when using precalculated incompleted propagations. 	modified:   run/gridchanger.sh 	modified:   run/input.dat 	deleted:    run/input.dat~ 	modified:   run/prop.dat 	modified:   run/run.sh 	modified:   run/run.sh~ 	modified:   src/MainMCE.f90 	modified:   src/bsetgen.f90 	modified:   src/propMCE.f90

--- run/gridchanger.sh
+++ run/gridchanger.sh
@@ -12,14 +12,14 @@ for grsp in `seq 1.20 0.01 1.50`; do
  if [ $(( $dim1 % 2 )) == 1 ]; then dim1=$[$dim1+1]; fi
  if [ $(( $dim2 % 2 )) == 1 ]; then dim2=$[$dim2+1]; fi
  dim3=$(( $dim1 * $dim2 ))
- sed -i "s/^qsizez.*/qsizez $dim1/g" input.dat
- sed -i "s/^psizez.*/psizez $dim2/g" input.dat
- sed -i "s/^gridsp.*/gridsp $grsp/g" input.dat
- sed -i "s/^in_nbf.*/in_nbf $dim3/g" input.dat 
+ sed -i "s/^qsizez.*/qsizez $dim1/g" input2.dat
+ sed -i "s/^psizez.*/psizez $dim2/g" input2.dat
+ sed -i "s/^gridsp.*/gridsp $grsp/g" input2.dat
+ sed -i "s/^in_nbf.*/in_nbf $dim3/g" input2.dat 
  for j in `seq 25 25 1000`; do
-  sed -i "s/^ALCMP.*/ALCMP $j/g" input.dat
-  sed -i "s/^Runfolder.*/Runfolder ${dim1}x${dim2}-$grsp-$j/g" input.dat
+  sed -i "s/^ALCMP.*/ALCMP $j/g" input2.dat
+  sed -i "s/^Runfolder.*/Runfolder ${dim1}x${dim2}-$grsp-$j/g" input2.dat
    i=$[$i+1]
-  cp input.dat ./calibinputs/input.$i
+  cp input2.dat ./calibinputs/input.$i
  done
 done 
